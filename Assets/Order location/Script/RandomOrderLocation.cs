902548664d5463a77efa611e3b81836f2a249dc5
sarawut-alt <sarawut.dety@bumail.net>
2025-08-31 21:18:55 +0700

    Added randomize pizza store location

--- Assets/Order location/Script/RandomOrderLocation.cs
+++ Assets/Order location/Script/RandomOrderLocation.cs
@@ -5,14 +5,28 @@ public class RandomOrderLocation : MonoBehaviour
 {
     public GameObject[] PossibleDeliveryLocation;
     public GameObject DeliveryLocationPrefab;
+    public GameObject PizzaStorePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int randomDeliveryLocation = UnityEngine.Random.Range(0, PossibleDeliveryLocation.Length);
+        int randomDeliveryLocation;
+        int randomPizzaStoreLocation;
+        do
+        {
+            randomDeliveryLocation = UnityEngine.Random.Range(0, PossibleDeliveryLocation.Length);
+            randomPizzaStoreLocation = UnityEngine.Random.Range(0, PossibleDeliveryLocation.Length);
+        } while (randomDeliveryLocation == randomPizzaStoreLocation);
+
+        //delivery location
         Vector3 deliveryLocation = PossibleDeliveryLocation[randomDeliveryLocation].transform.position;
         deliveryLocation.y = 0;
 
+        //recieve pizza location
+        Vector3 pizzaStoreLocation = PossibleDeliveryLocation[randomPizzaStoreLocation].transform.position;
+        pizzaStoreLocation.y = 0;
+
         Instantiate(DeliveryLocationPrefab, deliveryLocation, quaternion.identity);
+        Instantiate(PizzaStorePrefab, pizzaStoreLocation, quaternion.identity);
 
     }
 
